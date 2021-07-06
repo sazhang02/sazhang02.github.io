4b4afa5643dd8007fadbf0d7bfe7f53a4a08cb3d
Samantha Zhang <sjz46@cornell.edu>
2021-07-05 20:00:23 -0400

    added set up for projects & embedded figma + video

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
 var slide_index = 1;
 var demo1_active = false;
+var demo2_active = false;
+var video_active = false;
 show_slides(slide_index);
 
 function plus_slides(n) {
@@ -8,6 +10,37 @@ function plus_slides(n) {
 }
 function current_slide(n) {
   show_slides(slide_index = n);
+}
+
+function close_demo(n) {
+  var figma = document.getElementsByClassName("figma");
+  figma[n].style.display = "none";
+  if (n == 0) {
+    demo1_active = false;
+  }
+  else {
+    demo2_active = false;
+  }
+}
+
+function close_vid(n) {
+  var video = document.getElementsByClassName("video");
+  video[n].style.display = "none";
+  video_active = false;
+}
+
+function close_all() {
+  var figma = document.getElementsByClassName("figma");
+  var video = document.getElementsByClassName("video");
+  for (var i = 0; i < figma.length; i++) {
+    figma[i].style.display = "none";
+  }
+  for (var i = 0; i < video.length; i++) {
+    video[i].style.display = "none";
+  }
+  video_active = false;
+  demo1_active = false;
+  demo2_active = false;
 }
 
 function show_slides(n) {
@@ -26,16 +59,37 @@ function show_slides(n) {
   dots[slide_index - 1].className += " active";
 }
 
-function display_insta(n) {
+function display_figma(n) {
   var figma = document.getElementsByClassName("figma");
   if (n == 0) {
     if (demo1_active) {
-      figma[0].style.display = "none";
+      figma[n].style.display = "none";
     }
     else {
-      figma[0].style.display = "block";
+      figma[n].style.display = "block";
     }
-
+    demo1_active = !demo1_active;
   }
-  demo1_active = !demo1_active;
+  else if (n == 1) {
+    if (demo2_active) {
+      figma[n].style.display = "none";
+    }
+    else {
+      figma[n].style.display = "block";
+    }
+    demo2_active = !demo2_active;
+  }
+
+}
+function display_video(n) {
+  var video = document.getElementsByClassName("video");
+  if (n == 0) {
+    if (video_active) {
+      video[0].style.display = "none";
+    }
+    else {
+      video[0].style.display = "block";
+    }
+  }
+  video_active = !video_active;
 }
